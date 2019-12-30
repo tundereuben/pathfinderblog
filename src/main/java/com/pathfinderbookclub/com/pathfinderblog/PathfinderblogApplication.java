a6598010b0f17a0182933e6288c16ae44502a8ac
Tunde Ogunjimi <tundereuben@yahoo.com>
2019-12-30 13:20:03 +0100

    First commit

---
+++ src/main/java/com/pathfinderbookclub/com/pathfinderblog/PathfinderblogApplication.java
@@ -0,0 +1,13 @@
+package com.pathfinderbookclub.com.pathfinderblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pathfinderbookclub/com/pathfinderblog/PathfinderblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathfinderbookclub.com.pathfinderblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
